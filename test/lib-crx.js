d4d66899ccf4bbc39f20516ee4ea119bde35c10b
Frank <phranck+github@gmail.com>
2016-03-07 23:29:28 -0500

    Fixes issue where grunt-crx will remove entire directories. Fixes oncletom/grunt-crx#60.

--- test/lib-crx.js
+++ test/lib-crx.js
@@ -20,6 +20,7 @@ describe('lib/crx', function(){
         "standard": extensionHelper.getTaskConfiguration('test-standard'),
         "codebase": extensionHelper.getTaskConfiguration('test-codebase'),
         "exclude":  extensionHelper.getTaskConfiguration('test-exclude'),
+        "removal":  extensionHelper.getTaskConfiguration('test-removal'),
         "edge":     extensionHelper.getTaskConfiguration('test-edge'),
         "archive":  extensionHelper.getTaskConfiguration('test-archive')
       }
@@ -62,13 +63,30 @@ describe('lib/crx', function(){
 
       extensionHelper.build(crx, function () {
         //local
-        expect(grunt.file.expand('test/data/src/stuff/*')).to.have.lengthOf(1);
+        expect(grunt.file.expand('test/data/src/stuff/*')).to.have.lengthOf(2);
         expect(grunt.file.expand('test/data/src/*')).to.have.lengthOf(5);
 
         //copy
         expect(grunt.file.expand(path.join(crx.path + '/stuff/*'))).to.have.lengthOf(0);
         expect(grunt.file.expand(path.join(crx.path + '/*'))).to.have.lengthOf(4);
 
+        done();
+      });
+    });
+
+    it.only('should not remove entire directories', function(done) {
+      var crx = extensionHelper.createObject(getTaskConfig('removal'));
+      //console.log(crx);
+
+      extensionHelper.build(crx, function () {
+        var jsZipFile = new JSZip();
+        var dest = crx.zipDest;
+
+        expect(grunt.file.exists(dest), 'zip file should have been created').to.be.true;
+
+        jsZipFile.load(fs.readFileSync(dest));
+
+        expect(jsZipFile.file('stuff/awesome.txt')).to.be.ok;
         done();
       });
     });
